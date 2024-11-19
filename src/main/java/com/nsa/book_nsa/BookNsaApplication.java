f6d615abb4c78f560928d182d943500ce8c474d3
alicia bazin <bazin.alicia88@gmail.com>
2024-11-19 10:56:00 +0100

    création du projet en spring

---
+++ src/main/java/com/nsa/book_nsa/BookNsaApplication.java
@@ -0,0 +1,13 @@
+package com.nsa.book_nsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookNsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nsa/book_nsa/BookNsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsa.book_nsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
