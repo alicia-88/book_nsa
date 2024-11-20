82f4f4af552c53df9c9b09bd3ebe67ed6dc1acf6
SalahBey <salahzah8@gmail.com>
2024-11-20 10:31:32 +0100

    ajout du fichier Author.java

---
+++ src/main/java/com/nsa/book_nsa/model/Author.java
@@ -0,0 +1,98 @@
+package com.nsa.book_nsa.model;
+
+import java.util.Date;
+import jakarta.validation.constraints.*;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Author") // Associe cette classe à la table SQL "Author"
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-incrémentation
+	@Column(name = "aut_id")
+	private int id;
+
+	@NotNull 
+	@Column(name = "aut_lastname", nullable = false, length = 250)
+	@NotBlank(message = "Last name cannot be blank") // Non vide et non nul
+	@Size(max = 250, message = "Last name must be at most 250 characters")
+	private String lastName;
+
+	@Column(name = "aut_firstname", nullable = false, length = 250)
+	@NotBlank(message = "First name cannot be blank")
+	@Size(max = 250, message = "First name must be at most 250 characters")
+	private String firstName;
+	
+	@Column(name = "aut_birtdate", nullable = false)
+	@NotBlank(message = "Birth date cannot be null")
+	@Past(message = "Birth date must be in the past") // Doit être une date passée
+	@Temporal(TemporalType.DATE)
+	private Date birthDate;
+	
+	@Column(name = "aut_nationality", nullable = false, length = 250)
+	@NotBlank(message = "Nationality cannot be blank")
+	@Size(max = 250, message = "Nationality must be at most 250 characters")
+	private String nationality;
+	
+	// Constructeurs
+
+	public Author() {
+	}
+
+	public Author(String lastName, String firstName, Date birthDate, String nationality) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.birthDate = birthDate;
+		this.nationality = nationality;
+	}
+
+	// Getters et Setters
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	// Méthode toString pour afficher l'objet sous forme de chaîne
+	@Override
+	public String toString() {
+		return "Author{" + "id=" + id + ", lastName='" + lastName + '\'' + ", firstName='" + firstName + '\''
+				+ ", birthDate=" + birthDate + ", nationality='" + nationality + '\'' + '}';
+	}
+}
